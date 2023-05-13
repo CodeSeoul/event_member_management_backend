6b04ecb1ce4c69f003bc8fdb3849f7debe5b20e9
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2023-05-13 22:10:07 +0900

    feat: Fixed CORS for tests plus cleanup

--- src/main/java/org/codeseoul/event_member_management/EventMemberManagementApplication.java
+++ src/main/java/org/codeseoul/event_member_management/EventMemberManagementApplication.java
@@ -4,12 +4,9 @@ import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class EventMemberManagementApplication {
